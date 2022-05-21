94c76bea4a4650989544075111462308ed448873
Ananda Muhamad Lukman <v-ananda.lukman@anteraja.id>
2022-05-21 19:51:09 +0700

    Belajar Kotlin Nullable dengan not null assertions

---
+++ app/src/main/java/com/course/masterclass12/storybook/13_NotNullAssertions.kt
@@ -0,0 +1,25 @@
+package com.course.masterclass12.storybook
+
+/**
+ * Nullables - Elvis Operator - Not Null Assertions
+ * Operator Elvis merupakan ?:
+ */
+
+fun main() {
+    val name = "Lukman"
+    val age = 21
+    getName(name, age)
+}
+
+/**
+ * Jadi tanda !! atau not null assertions maksudnya adalah
+ * pada variabel tersebut dapat dipastikan 100% tidak akan null!
+ * kalau masih ada kemungkinan null maka berikan ? atau berikan kondisi if ( varibel != null) { isi jika ada isi } else { isi kalau null }
+ */
+fun getName(name: String?, age: Int?) {
+    if (name!!.isNotEmpty()) {
+        name.uppercase()
+        age!! + 1
+    }
+    println("Nama ku adalah $name umurku sekarang $age")
+}
